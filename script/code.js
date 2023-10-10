6acbb83dba665ba5cc9c973a01b59f8bb196179b
JA77776 <jaydonnhendricks09@gmail.com>
2023-10-10 10:42:32 +0200

    index.html

---
+++ script/code.js
@@ -0,0 +1,6 @@
+let firstname = 'Jayd-donn'
+console.log("My name is Jay-donn" + firstname)
+console.log(firstname + "have 1 sister and 4 brothers")
+
+console.log(`My name is + ${firstname}`)
+console.log(`${firstname} + have 1 sister and 4 brothers`)
